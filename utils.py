3c7df41d3280c0b0f527f947c3ca67db9acea32d
Nichole Corpron <215328826+ncorpron@users.noreply.github.com>
2025-09-04 08:06:58 -0400

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+import random, numpy as np
+import torch
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
